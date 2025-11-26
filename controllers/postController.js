631e324a407c6f1bd86bcc3d858e5e33ea62dce4
stefanobellomo <stefanobellomo1998@gmail.com>
2025-11-26 14:35:58 +0100

    add error messsage in show if not found id

--- controllers/postController.js
+++ controllers/postController.js
@@ -3,24 +3,33 @@ const posts = require('../data/listPosts')
 function index(req, res) {
 
     const tags = req.query.tags;
-    console.log(tags);
+    // console.log(tags);
 
     let filteredTags = posts
-    console.log(filteredTags);
+    // console.log(filteredTags);
 
-    if (tags) {
-        filteredTags = posts.filter(post => post.tags.includes(tags))
-        console.log(filteredTags);
-    }
+    // if (tags) {
+    //     filteredTags = posts.filter(post => post.tags.includes(tags))
+    //     console.log(filteredTags);
+    // }
 
     res.json(filteredTags)
 }
 
 function show(req, res) {
+
     const id = Number(req.params.id)
     const post = posts.find((post) => (id === post.id))
 
-    res.json(filteredTags)
+    // se non trovi il post...
+    if (!post) {
+        return res.status(404).json({
+            error: true,
+            message: 'Post not found'
+        })
+    }
+
+    res.json(post)
 }
 
 function store(req, res) {
